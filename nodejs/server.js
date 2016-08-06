526da19465a702da477e698bf0e05ed2a2704287
56 <56-n@56s-MacBook-Air.local>
2016-08-06 16:21:19 +0900

    HTML ファイルの読み込み

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,27 +1,20 @@
 // Webサーバー
-var http = require('http'),
-    fs = require('fs');
+var http = require('http'),fs = require('fs');
 var settings = require('./settings');
 console.log(settings);
 var server = http.createServer();
 server.on('request', function(req, res){
-  //アクセスしたURLに応じて処理を変える
-  //アクセスしてきたURLは req.url で取ることができる
-  switch (req.url) {
-    case '/about':
-      msg = "about this page";
-      break;
-    case '/profile':
-      msg = "about me";
-      break;
-    default:
-      msg = 'wrong page';
-      break;
-  }
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('hello from' + req.url + '\n');
-  res.write(msg);
+  fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, data){
+    //エラー処理
+    if(err) {
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.write("not found!");
+      return res.end();
+    }
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(data);
   res.end();
-})
+  });
+});
 server.listen(settings.port, settings.host);
 console.log("server listening ...");
